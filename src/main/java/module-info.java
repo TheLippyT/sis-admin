2ab818de0b6e4e430db1710ffa3cd9bf98673c18
TheLippyT <44298235+TheLippyT@users.noreply.github.com>
2022-11-14 08:39:26 -0300

    Add files via upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module sr.sis.sisadmin {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens sr.sis.sisadmin to javafx.fxml;
+    exports sr.sis.sisadmin;
+}
